f869b2748c8971a2830c504bb39fb396791c1ef6
julioMarvim <juliomarvim@yahoo.com.br>
2020-05-24 02:31:09 -0300

    Criação da controller UserController e do método de criação de usuários.

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Julio Marvim',
-    email: 'juliomarvim@yahoo.com.br',
-    password_hash: '1235689900',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
